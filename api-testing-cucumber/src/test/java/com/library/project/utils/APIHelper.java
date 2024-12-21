7687b7a6637f4ea239cbc95c8373fa65add5b0d3
lakshan-cd <87594213+lakshan-cd@users.noreply.github.com>
2024-12-22 04:13:23 +0530

    Helper method for stepdefinitions

---
+++ api-testing-cucumber/src/test/java/com/library/project/utils/APIHelper.java
@@ -0,0 +1,41 @@
+package com.library.project.utils;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.springframework.stereotype.Component;
+
+@Component
+public class APIHelper {
+
+    // Method to send a POST request with Basic Authentication
+    public Response sendPostRequestWithBasicAuth(String endpoint, String requestBody, String username, String password) {
+        return RestAssured.given()
+                .auth().basic(username, password) // Add Basic Auth header
+                .header("Content-Type", "application/json") // Set Content-Type
+                .body(requestBody) // Add request body
+                .post(endpoint); // Perform POST request
+    }
+
+    public Response sendPutRequestWithBasicAuth(String endpoint, String requestBody, String username, String password) {
+        return RestAssured.given()
+                .auth().basic(username, password)
+                .header("Content-Type", "application/json")
+                .body(requestBody)
+                .put(endpoint);
+    }
+
+    public Response sendDeleteRequestWithBasicAuth(String endpoint, String username, String password) {
+        return RestAssured.given()
+                .auth().basic(username, password)
+                .header("Content-Type", "application/json")
+                .delete(endpoint);
+    }
+
+    public Response sendGetRequestWithBasicAuth(String endpoint, String username, String password) {
+        return RestAssured.given()
+                .auth().basic(username, password)
+                .header("Content-Type", "application/json")
+                .get(endpoint);
+    }
+
+}
