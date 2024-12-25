d97a204115b52e1eb3fb6f4e156e9b4aa29f3fb0
lakshan-cd <87594213+lakshan-cd@users.noreply.github.com>
2024-12-25 14:38:48 +0530

    Refactor configurations

--- api-testing-cucumber/src/test/java/com/library/project/runners/CucumberTestRunnerTest.java
+++ api-testing-cucumber/src/test/java/com/library/project/runners/CucumberTestRunnerTest.java
@@ -1,10 +1,12 @@
 package com.library.project.runners;
 
+import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
+import org.junit.runner.RunWith;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
@@ -14,5 +16,16 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @SelectPackages("com.library.project")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.library.project.stepdefinitions")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
-public class CucumberTestRunner {
+@CucumberOptions(
+        features = "src/test/resources/features/book_post.feature",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports.html",
+                "json:target/cucumber-reports/cucumber.json",
+                "junit:target/cucumber-reports/cucumber.xml",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        },
+        monochrome = true
+)
+public class CucumberTestRunnerTest {
 }
