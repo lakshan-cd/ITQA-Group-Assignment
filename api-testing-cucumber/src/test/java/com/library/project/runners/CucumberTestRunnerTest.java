b448d70b790e4a725d49b0badc822fa453a470f4
lakshan-cd <87594213+lakshan-cd@users.noreply.github.com>
2024-12-25 18:26:29 +0530

    Added Allure report configurations

--- api-testing-cucumber/src/test/java/com/library/project/runners/CucumberTestRunnerTest.java
+++ api-testing-cucumber/src/test/java/com/library/project/runners/CucumberTestRunnerTest.java
@@ -2,22 +2,12 @@ package com.library.project.runners;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
 import org.junit.runner.RunWith;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-
-@Suite
-@IncludeEngines("cucumber")
-@SelectPackages("com.library.project")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.library.project.stepdefinitions")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
+@RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/book_post.feature",
+        features = "src/test/resources/features",
+        glue = {"com.library.project.stepdefinitions", "com.library.project"},
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports.html",
