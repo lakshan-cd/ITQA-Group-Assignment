3371ac0f0940df6a0da1083c839a2431a25eeac9
Sasadari <sasadarishashiprabha@gmail.com>
2025-01-01 21:43:09 +0530

    Verify "Exclude Stock Out" Button Displays Only In-Stock Products

--- ui-tesing-playwright/tests/sasadariTest.spec.js
+++ ui-tesing-playwright/tests/sasadariTest.spec.js
@@ -3,7 +3,7 @@ const { POManager } = require("../pageobjects/POManager")
 
 
 
-test.only('@web @sample test sdssss ', async ({ browser }) => {
+test('@web @sample check product price odering ', async ({ browser }) => {
     const context = await browser.newContext();
     const page = await context.newPage();
 
@@ -20,3 +20,43 @@ test.only('@web @sample test sdssss ', async ({ browser }) => {
     expect(isSortedAscending).toBeTruthy();
 });
 
+
+
+test('@web @sample Verify a brand Displays Only that brand Products ', async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://www.singersl.com/brands");
+    await page.locator('img[alt="ASUS"]').click();
+    const brandName = (await page.locator("#block-breadcrumbs li").nth(2).textContent())
+        .toLowerCase()
+        .trim();
+    const productNames = await page.locator(".product-item h2").allTextContents();
+    const allIncludeBrandName = productNames.every(productName =>
+        productName.toLowerCase().includes(brandName)
+    );
+    expect(allIncludeBrandName).toBeTruthy();
+    console.log(allIncludeBrandName);
+});
+
+test('@web @sample Verify "Exclude Stock Out" Button Displays Only In-Stock Products', async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://www.singersl.com/brands/asus");
+    await page.locator("label:has-text('Exclude Stock Out')").click();
+    const soldOutProducts = await page.locator(".sold-out").count();
+    expect(soldOutProducts).toBe(0);
+});
+
+
+// test.only('@web @sample test sdssssddd ', async ({ browser }) => {
+//     const context = await browser.newContext();
+//     const page = await context.newPage();
+//     await page.goto("https://www.singersl.com/product/unic-rice-cooker-28l-1000w-urc28-16e");
+//     const preProductPrice=await page.locator(".selling-price .data").textContent();
+//     await page.locator(".product-variant-anchor").first().click();
+//     const newProductPrice=await page.locator(".selling-price .data").textContent();
+// console.log(preProductPrice,newProductPrice);
+
+
+  
+// });
