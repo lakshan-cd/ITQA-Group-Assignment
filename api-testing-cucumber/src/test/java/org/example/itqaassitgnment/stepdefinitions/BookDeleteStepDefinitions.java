ef5a017a9fbe54ed3aebe46c9ff625e328bf7cbe
Chamodi Hansika <hansibagya430@gmail.com>
2025-01-03 18:15:27 +0530

    DELETE API testing

---
+++ api-testing-cucumber/src/test/java/org/example/itqaassitgnment/stepdefinitions/BookDeleteStepDefinitions.java
@@ -0,0 +1,41 @@
+package org.example.itqaassitgnment.stepdefinitions;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+public class BookDeleteStepDefinitions {
+
+    private Response response;
+    private RequestSpecification request;
+    private String endpoint;
+    private String username;
+    private String password;
+
+    @Given("the API endpoint is {string}")
+    public void theApiEndpointIs(String apiEndpoint) {
+        this.endpoint = apiEndpoint;
+        RestAssured.baseURI = "http://localhost:8080";
+    }
+
+    @Given("the Basic Authentication username is {string} and password is {string}")
+    public void theBasicAuthenticationUsernameIsAndPasswordIs(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.request = RestAssured.given().auth().basic(username, password);
+    }
+
+    @When("I send a DELETE request with the ID {int}")
+    public void iSendADeleteRequestWithTheID(int bookId) {
+        response = request.delete(endpoint.replace("{id}", String.valueOf(bookId)));
+    }
+
+    @Then("the response status should be {int}")
+    public void theResponseStatusShouldBe(int expectedStatusCode) {
+        Assert.assertEquals(expectedStatusCode, response.getStatusCode());
+    }
+}
