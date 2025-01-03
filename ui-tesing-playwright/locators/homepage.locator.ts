21c2278b61a88fc1c8f00978308122ee26387e64
lakshan-cd <87594213+lakshan-cd@users.noreply.github.com>
2025-01-03 18:14:37 +0530

    Common locators and home page implementation class

---
+++ ui-tesing-playwright/locators/homepage.locator.ts
@@ -0,0 +1,6 @@
+export class HomePageLocators {
+    public static CURRENCY_SELECTION: string = '#currency-change-select';
+    public static PRODUCT_PRICE: string = '.tabs-content-items .price';
+    public static SEARCHBAR_ID : string = "#edit-search";
+    public static SEARCHED_RESULT : string = ".product-listing-content .sku";
+}
