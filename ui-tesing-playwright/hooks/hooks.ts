f0a8607ee900df6e4f97b27011029334c245169b
UserSampath <nalakasampathsmp@gmail.com>
2025-01-03 22:21:48 +0530

    fix errors

--- ui-tesing-playwright/hooks/hooks.ts
+++ ui-tesing-playwright/hooks/hooks.ts
@@ -4,20 +4,16 @@ import {PlaywrightConfig} from "../utils/playwright.config";
 const playwrightConfig:PlaywrightConfig = PlaywrightConfig.getInstance();
 
 BeforeAll(async function () {
-    console.log('Global setup: Initializing Playwright browser and context.');
-    const page = await playwrightConfig.getPage(); // Initialize the Playwright page object
-    console.log('Page initialized:', await page.title());
-    console.log('***********************************************************');
+
+    console.log('* * * * * * * * * * * *');
 });
 AfterAll(async function () {
     await playwrightConfig.closePage();
-    console.log('Global teardown: Closing Playwright browser and context.');
 });
 
 After(async function ({pickle}) {
-    const page = await playwrightConfig.getPage(); // Initialize the Playwright page object
+    const page = await playwrightConfig.getPage();
     const img = await page.screenshot({path: `./reports/screenshots/${pickle.name}.png`, type: 'png'});
     this.attach(img, 'image/png');
-    console.log('\n***********************************************************\n');
-
+    console.log("\n* * * * * * * * * * * *\n");
 });
