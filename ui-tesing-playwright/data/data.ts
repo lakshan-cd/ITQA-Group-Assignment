03788a086bb84d95587489462a15270525b96926
UserSampath <nalakasampathsmp@gmail.com>
2025-01-03 23:02:34 +0530

    delete un wanted files

--- ui-tesing-playwright/data/data.ts
+++
@@ -1,7 +0,0 @@
-import {SharedData} from "./shared/SharedData";
-import {AuthData} from "./authentication/AuthData";
-
-export const data = {
-    SharedData,
-    AuthData,
-}
--- ui-tesing-playwright/model/authentication/registration.model.ts
+++
@@ -1,7 +0,0 @@
-export interface RegistrationModel {
-
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-}
